6a335d27d3aaae79722f4d86842041bf450a47f1
Abdullah-Sakib <abdullahsakib888@gmail.com>
2022-10-25 20:36:17 +0600

    Created the course details API

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const app = express()
 const port = process.env.PORT || 5000;
 const courses = require('./data/courses.json')
 const cors = require('cors');
+const courseDetails = require('./data/courseDetails.json')
 
 app.use(cors());
 
 app.get('/', (req, res)=>{
   res.send(courses)
+})
+
+app.get('/details/:id', (req, res)=>{
+  const id = req.params.id;
+  const details = courseDetails.find(course => course.id == id);
+  res.send(details);
 })
 
 app.listen(port, ()=>{
